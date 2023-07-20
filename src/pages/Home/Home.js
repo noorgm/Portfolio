685715b1372bbf7023586441b1d5349e62f6be80
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-20 22:37:40 +0100

    some changes on home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,47 +1,45 @@
 import React from 'react';
+
+
+import portrait from '../../assets/images/portrait1.png'
 import './home.css';
-import circle from '../../assets/images/circle-arrow2.png';
-import portrait from '../../assets/images/portraite.png';
+
+
 const Home = () => {
   return (
-    <main className="home-bg">
+    <main className="home">
       <section className="container">
-        <div className="home grid">
-          <section className="home-left">
-            <h1>Hey, My name is <span className='name'>Noor</span>ullah</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus minima repellat voluptatum magni. Lo suscipit praesentium ex sit.</p>
-          </section>
-          <section className="hl-bottom">
-            <img src={circle} alt="circle" />
-            <button>About Me</button>
+        <div className="main-home grid">
+
+          <section className="left-home">
+            <div className="l-h-top">
+              <h1>Noorullah GM</h1>
+              <h3><span className='front-end'>Front-End</span> Developer</h3>
+            </div>
+            <div className="l-h-middle">
+              <div className="home-skill">
+                <span>icon</span>
+                <h3>UI Design</h3>                
+              </div>
+              <div className="home-skill">
+                <span>icon</span>
+                <h3>Responsive Design</h3>                
+              </div>
+              <div className="home-skill">
+                <span>icon</span>
+                <h3>Git</h3>                
+              </div>
+            </div>
+            <div className="home-btns">
+              <button>View My Work</button>
+              <p>Download CV</p>
+            </div>
           </section>
 
-          <section className="portrait">
-            <img src={portrait} alt="my-portrait" />
+          <section className="right-home">
+            <img src={portrait} alt="my portraite" />
           </section>
 
-          <section className="home-skills">
-            {/* <div className="box-skill">
-              <img src={git} alt="git" />
-              <p>Git</p>
-            </div>
-            <div className="box-skill">
-              <img src={figma} alt="figma" />
-              <p>Figma</p>
-            </div>
-            <div className="box-skill">
-              <img src={xd} alt="adobe xd" />
-              <p>Adobe XD</p>
-            </div>
-            <div className="box-skill">
-              <img src={mui} alt="mui" />
-              <p>Material UI</p>
-            </div>
-            <div className="box-skill">
-              <img src={ai} alt="adobe ai" />
-              <p>Adobe Illustrator</p>
-            </div> */}
-          </section>
         </div>
       </section>
     </main>
