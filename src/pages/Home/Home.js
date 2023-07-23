4378c4371445e436ff599d69719103d5b62ef316
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-23 17:11:24 +0100

    home page layout sorta done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 
 import portrait from '../../assets/images/portrait1.png'
+import ui from '../../assets/images/ui.png'
+import responsive from '../../assets/images/laptop.png'
+import git from '../../assets/images/git.png'
 import './home.css';
 
 
@@ -14,25 +17,28 @@ const Home = () => {
           <section className="left-home">
             <div className="l-h-top">
               <h1>Noorullah GM</h1>
-              <h3><span className='front-end'>Front-End</span> Developer</h3>
+              <h2><span className='front-end'>Front-End</span> Developer</h2>
             </div>
             <div className="l-h-middle">
               <div className="home-skill">
-                <span>icon</span>
-                <h3>UI Design</h3>                
+                <img src={ui} alt="ui design" />
+                <h5>UI Design</h5>                
               </div>
               <div className="home-skill">
-                <span>icon</span>
-                <h3>Responsive Design</h3>                
+                <img src={responsive} alt="responsive design" />
+                <h5>Responsive Design</h5>                
               </div>
               <div className="home-skill">
-                <span>icon</span>
-                <h3>Git</h3>                
+                <img src={git} alt="git" />
+                <h5>Git</h5>                
               </div>
+            </div>
+            <div className="home-description">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi asperiores eius amet, cumque minima deleniti eveniet quas quasi dignissimos odit exercitationem in.</p>
             </div>
             <div className="home-btns">
               <button>View My Work</button>
-              <p>Download CV</p>
+              <button>Download CV</button>
             </div>
           </section>
 
