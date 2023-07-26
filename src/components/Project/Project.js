cd7f5a09de785170809c55db4731f17527dd2bbc
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-26 14:09:31 +0100

    Did not like the look of Project componnet so got rid of all the codes and updated it again, just need to do its CSS and some javscript then its done but thats after I come back from break

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 
 import NorthEastIcon from '@mui/icons-material/NorthEast';
-import mobile from '../../assets/images/mobile.png';
 import GitHub  from '@mui/icons-material/GitHub';
+import carLogo from '../../assets/images/logoo.svg';
+import carSs from '../../assets/images/car-ss.png';
 import './project.css';
 
 const Project = () => {
@@ -11,72 +12,29 @@ const Project = () => {
     <section className="project-bg">
       <div className="container">
         <h3 className='project-heading'>Projects</h3>
+
         <div className="projects grid">
-
-          <div className="project-box container">
-            <div className="project-box-heading">
-              <div className="p-b-h-left">
-                <h4>Car Hire</h4>
-                <p>Cars for hire in Yorkshire</p>
-              </div>
-              <div className="p-b-h-right">
-                <GitHub className='project-github-icon' />
-                <NorthEastIcon className='project-arrow-icon'/>
-              </div>
-            </div>
-            <div className="project-box-img">
-              <img src={mobile} alt="mobile" />
-            </div>
+          <div className="project-left">
+            <img src={carSs} alt="car screenshot" />
           </div>
 
-          <div className="project-box container">
-            <div className="project-box-heading">
-              <div className="p-b-h-left">
-                <h4>Car Hire</h4>
-                <p>Cars for hire in Yorkshire</p>
-              </div>
-              <div className="p-b-h-right">
-                <GitHub className='project-github-icon' />
-                <NorthEastIcon className='project-arrow-icon'/>
+          <div className="project-right">
+            <div className="p-r-heading">
+              <h3>Car Hire</h3>
+              <img src={carLogo} alt="car logo" />
+            </div>
+            <div className="p-r-middle">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum iste voluptatibus asperiores vero. Voluptas deleniti sequi ducimus itaque nostrum, vitae similique, voluptatibus delectus molestiae fugit, repudiandae quos necessitatibus unde eius. Non!</p>
+              <div className="p-r-lib">
+                <p>Lorem, ipsum</p>
+                <p>Lorem, ipsum</p>
               </div>
             </div>
-            <div className="project-box-img">
-              <img src={mobile} alt="mobile" />
-            </div>
-          </div>
-
-          <div className="project-box container">
-            <div className="project-box-heading">
-              <div className="p-b-h-left">
-                <h4>Car Hire</h4>
-                <p>Cars for hire in Yorkshire</p>
-              </div>
-              <div className="p-b-h-right">
-                <GitHub className='project-github-icon' />
-                <NorthEastIcon className='project-arrow-icon'/>
-              </div>
-            </div>
-            <div className="project-box-img">
-              <img src={mobile} alt="mobile" />
+            <div className="p-r-bottom">
+              <GitHub className='project-github-icon' />
+              <NorthEastIcon className='project-arrow-icon'/>
             </div>
           </div>
-
-          <div className="project-box container">
-            <div className="project-box-heading">
-              <div className="p-b-h-left">
-                <h4>Car Hire</h4>
-                <p>Cars for hire in Yorkshire</p>
-              </div>
-              <div className="p-b-h-right">
-                <GitHub className='project-github-icon' />
-                <NorthEastIcon className='project-arrow-icon'/>
-              </div>
-            </div>
-            <div className="project-box-img">
-              <img src={mobile} alt="mobile" />
-            </div>
-          </div>
-
         </div>
 
       </div>
