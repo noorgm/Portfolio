acc7944f18033e0a4e7bb53fbe0c549a9ff0922b
noorgm <97991519+noorgm@users.noreply.github.com>
2023-07-28 00:25:11 +0100

    Big mistake, I should have done the mobile first and go up , but instead done the desktop first. So got rid of all the css I worked for the past week or so

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-scroll';
 
+import MenuIcon from '@mui/icons-material/Menu';
 import './navbar.css';
 
 const Navbar = () => {
 
+  const [openMenu, setOpenMenu] = useState(false);
+  
+  const menuIconHandler = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const menuIconNavHandler = () => {
+    setOpenMenu(false);
+  };
+
   return (
     <section className='container'>
-      <nav>
-        <Link to='home' spy={true} smooth={true} offset={50} duration={500} className='nav-logo'>
+      <nav className='grid'>
+        <Link to='home' spy={true} smooth={true} offset={-100} duration={500} className='nav-logo' onClick={menuIconNavHandler}>
           <h4>logo here</h4>
         </Link>
 
-        <div className='nav-ul'>
-          <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
-          <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>About Me</Link>
-          <Link to="project" spy={true} smooth={true} offset={-100} duration={500} >Projects</Link>       
+        <div className={`menu-icon ${openMenu ? 'active' : ''}`} onClick={menuIconHandler}>
+          <MenuIcon />
         </div>
 
-        <Link className='nav-sign' to="contact" spy={true} smooth={true} offset={-100} duration={500}>
-          <button>Contact Me</button>
-        </Link>     
+        <div className={`nav-ul ${openMenu ? 'active' : ''}`}>
+          <Link to="home" className='items' spy={true} smooth={true} offset={100} duration={500} onClick={menuIconNavHandler}>Home</Link>
+          <Link to="about" className='items' spy={true} smooth={true} offset={100} duration={500} onClick={menuIconNavHandler}>About Me</Link>
+          <Link to="project" className='items' spy={true} smooth={true} offset={100} duration={500} onClick={menuIconNavHandler}>Projects</Link>       
+          <Link className='items nav-contact' to="contact" spy={true} smooth={true} offset={100} duration={500} onClick={menuIconNavHandler}>
+            <button>Contact Me</button>
+          </Link>     
+        </div>
       </nav>
     </section>
   )
