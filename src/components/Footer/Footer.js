12af428766f8c1f5ffb40019d1ca2e72e01a310e
noorgm <97991519+noorgm@users.noreply.github.com>
2023-08-01 20:26:11 +0100

    Contact component done too

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-// import GitHub  from '@mui/icons-material/GitHub';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import MoodIcon from '@mui/icons-material/Mood';
+import GitHub  from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MoodIcon from '@mui/icons-material/Mood';
 import './footer.css';
 
 const Footer = () => {
   return (
     <section className="footer-bg">
-      <div className="grid">
-        Footer
+      <div className="footer grid">
+        <section className="footer-top">
+          <div className="footer-left">
+            <h4>Logo Here</h4>
+            <p className='footer-p-icon'>Thanks for visiting! <span className='smiley-icon'><MoodIcon /></span></p>
+          </div>
+          <div className="footer-right">
+            <GitHub className='f-r-icon' />
+            <LinkedInIcon className='f-r-icon' />
+          </div>
+        </section>
+
+        <section className="footer-bottom">
+          <p>Copyright &copy; 2023. All right reserved. Noorullah GM</p>
+        </section>
+
       </div>
     </section>
   )
