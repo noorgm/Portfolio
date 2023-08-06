25e921d0b7de41c09bb71fec018963413274ca1f
noorgm <97991519+noorgm@users.noreply.github.com>
2023-08-06 01:30:21 +0100

    sorted my logo out

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../assets/images/logo.png';
 import './navbar.css';
 
 const Navbar = () => {
@@ -21,7 +22,7 @@ const Navbar = () => {
     <section className='navbar'>
       <nav className='grid'>
         <Link to='home' spy={true} smooth={true} offset={-100} duration={500} className='nav-logo' onClick={menuIconNavHandler}>
-          <h4>logo here</h4>
+          <img src={logo} alt="logo" />
         </Link>
 
         <div className={`menu-icon ${openMenu ? 'active' : ''}`} onClick={menuIconHandler}>
